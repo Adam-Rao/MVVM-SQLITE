10e8725c655bfde32e096739b3e05772a89c6d1b
Adam Rao <raoadam87@gmail.com>
2019-10-16 20:26:49 +0300

    Initial Commit

---
+++ app/src/main/java/adam/rao/sqlite/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package adam.rao.sqlite.ui
+
+import adam.rao.sqlite.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
