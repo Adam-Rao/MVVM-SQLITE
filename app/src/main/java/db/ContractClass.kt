f1f723e3d33e3eb1ed9aafbf5dc809e9f5b78762
Adam Rao <raoadam87@gmail.com>
2019-10-16 21:34:43 +0300

    Adds Contract Class

---
+++ app/src/main/java/db/ContractClass.kt
@@ -0,0 +1,20 @@
+package db
+
+import android.provider.BaseColumns
+import android.provider.BaseColumns._ID
+
+class ContractClass {
+    companion object class Task(): BaseColumns {
+        companion object {
+            const val TABLE_NAME = "tasks"
+            const val TASK_COLUMN = "task"
+            const val COMPLETED_COLUMN = "task_completed"
+
+            const val CREATE_TASK_TABLE = "CREATE TABLE $TABLE_NAME (" +
+                    "$_ID INTEGER PRIMARY KEY, " +
+                    "$TASK_COLUMN VARCHAR(256), " +
+                    "$COMPLETED_COLUMN VARCHAR(256)" +
+                    ")"
+        }
+    }
+}
